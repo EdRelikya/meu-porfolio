c395a6ce06c8f86c65cff48c06dee3a97c520e18
Edmundo Cariolano Da silva <edmundo.00000859930@unicap.br>
2026-05-20 12:01:37 -0300

    Criando seção de skills

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,41 @@
+import SkillCard from "../components/SkillCard";
+
 export default function Home() {
   return (
-    <main className="hero">
-      <div className="hero-content">
-        <h1>Olá, eu sou Ed</h1>
+    <main>
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Olá, eu sou Ed</h1>
 
-        <h2>Desenvolvedor Front-End</h2>
+          <h2>Desenvolvedor Front-End</h2>
 
-        <p>
-          Desenvolvendo interfaces modernas, responsivas e experiências
-          digitais utilizando React, Next.js e JavaScript.
-        </p>
+          <p>
+            Desenvolvendo interfaces modernas, responsivas e experiências
+            digitais utilizando React, Next.js e JavaScript.
+          </p>
 
-        <div className="hero-buttons">
-          <button>Ver Projetos</button>
+          <div className="hero-buttons">
+            <button>Ver Projetos</button>
 
-          <button className="secondary-button">
-            GitHub
-          </button>
+            <button className="secondary-button">
+              GitHub
+            </button>
+          </div>
         </div>
-      </div>
+      </section>
+
+      <section className="skills-section">
+        <h2>Minhas Tecnologias</h2>
+
+        <div className="skills-grid">
+          <SkillCard title="HTML5" />
+          <SkillCard title="CSS3" />
+          <SkillCard title="JavaScript" />
+          <SkillCard title="React" />
+          <SkillCard title="Next.js" />
+          <SkillCard title="Git & GitHub" />
+        </div>
+      </section>
     </main>
   );
 }
---
+++ src/components/SkillCard.jsx
@@ -0,0 +1,7 @@
+export default function SkillCard({ title }) {
+  return (
+    <div className="skill-card">
+      <h3>{title}</h3>
+    </div>
+  );
+}
